bb2c30e811dd25a58e3936c319fdabcb398de1c6
tishan <tishan@tabbledabble.com>
2015-07-08 08:42:50 -0400

    Updated in-line license agreement.

--- Java/src/com/quicktapsurvey/apihelper/request/LoginRequest.java
+++ Java/src/com/quicktapsurvey/apihelper/request/LoginRequest.java
@@ -4,11 +4,12 @@ INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
 PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL QUICKTAPSURVEY OR TABBLEDABBLE INC. BE 
 LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE 
-USE OR OTHER DEALINGS IN THE SOFTWARE. USE AT YOUR OWN RISK.
+USE OR OTHER DEALINGS IN THE SOFTWARE. THE SOFTWARE MAY BE USED FOR ANY PURPOSE, 
+INCLUDING PAID-SERVICES, WITHOUT ANY ROYALTIES. USE AT YOUR OWN RISK.
 
 Date: July 5, 2015
 Author: Tishan Mills
-*/
+ */
 package com.quicktapsurvey.apihelper.request;
 
 public class LoginRequest {
@@ -16,7 +17,7 @@ public class LoginRequest {
 	String username;
 	String password;
 	String apiKey;
-	
+
 	/**
 	 * @param username
 	 * @param password
@@ -28,41 +29,50 @@ public class LoginRequest {
 		this.password = password;
 		this.apiKey = apiKey;
 	}
+
 	/**
 	 * @return the username
 	 */
 	public String getUsername() {
 		return username;
 	}
+
 	/**
-	 * @param username the username to set
+	 * @param username
+	 *            the username to set
 	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	/**
 	 * @return the password
 	 */
 	public String getPassword() {
 		return password;
 	}
+
 	/**
-	 * @param password the password to set
+	 * @param password
+	 *            the password to set
 	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	/**
 	 * @return the apiKey
 	 */
 	public String getApiKey() {
 		return apiKey;
 	}
+
 	/**
-	 * @param apiKey the apiKey to set
+	 * @param apiKey
+	 *            the apiKey to set
 	 */
 	public void setApiKey(String apiKey) {
 		this.apiKey = apiKey;
 	}
-	
+
 }
